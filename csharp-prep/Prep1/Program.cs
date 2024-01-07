e848ab8afc62664a13efbdca60c4dacba72e9c35
joatjoe <joelninr@gmail.com>
2024-01-07 19:26:02 -0400

    Done the first activity of Prep 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first name? "); // Ask for the first name.
+        string firts_name = Console.ReadLine(); // Save the first name in "first_name".
+        
+        Console.Write("What is your last name? "); // Ask for last name.
+        string last_name = Console.ReadLine(); // Save last name in "last_name".
+
+        Console.WriteLine(); // Line for aesthetic.
+
+        Console.WriteLine($"Your name is {last_name}, {firts_name} {last_name}."); // Print the info.
+
     }
 }
