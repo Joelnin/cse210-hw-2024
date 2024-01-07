fc5e4f14525644178859c2891c6014be4de7c612
joatjoe <joelninr@gmail.com>
2024-01-07 19:38:47 -0400

    Change comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         
         Console.Write("What is your first name? "); // Ask for the first name.
-        string firtsName = Console.ReadLine(); // Save the first name in "first_name".
+        string firtsName = Console.ReadLine(); // Save the first name in "firstName".
         
         Console.Write("What is your last name? "); // Ask for last name.
-        string lastName = Console.ReadLine(); // Save last name in "last_name".
+        string lastName = Console.ReadLine(); // Save last name in "lastName".
 
         Console.WriteLine(); // Line for aesthetic.
 
