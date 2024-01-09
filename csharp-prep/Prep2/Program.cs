f8d563a500d3f4b970377ac7aa0c62478de767ca
joatjoe <joelninr@gmail.com>
2024-01-08 23:02:56 -0400

    Make the code for If statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? "); // Ask for percentage.
+        string userInput = Console.ReadLine(); // Percentage in a str: UserInput.
+        float percentage = float.Parse(userInput); // Percentage in a float: percentage.
+        string letter; // Variable for grade letter: letter.
+        string decisionMessage; // Variable for failing/failing class message: decisionMessage.
+
+        // If's statements for deciding wich letter to use.
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        // If's statements for deciding if the kid passed the class. (Hope they do)
+
+        if (percentage >= 70)
+        {
+            decisionMessage = "Congratulations, you passed!";
+
+        }
+
+        else
+        {
+            decisionMessage = "Sorry, you haven't passed the course this time. \nNext time will be better, don't give up!";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        Console.WriteLine($"{decisionMessage}");
+
     }
 }
